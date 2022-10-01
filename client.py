88676ee9deb7c3c3552939b109cd240915c3a068
Vincentzyx <929403983@qq.com>
2022-10-01 22:12:33 +0800

    Update

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import os
 import argparse
+import multiprocessing as mp
 
-parser = argparse.ArgumentParser(description="分布式生成棋谱")
-parser.add_argument("--user", default="VinXiangQi", type=str, help="用于统计训练量的用户名")
-parser.add_argument("--threads", default=-1, type=int, help="用于跑谱的核心数")
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--user", default="colabb", type=str, help="")
+parser.add_argument("--threads", default=-1, type=int, help="")
 Args = parser.parse_args()
 
-os.system("chmod +x ./stockfish")
-os.system(f"./stockfish -u {Args.user} -t {Args.threads}")
+
+os.system("chmod +x ./colabb")
+os.system(f"./colabb -u {Args.user} -t {Args.threads}")
