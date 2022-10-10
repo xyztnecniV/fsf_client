e3f1c8bd444f99cad28eed9f1b07a0381a74d22b
Vincentzyx <929403983@qq.com>
2022-10-11 01:00:34 +0800

    Pika

--- client.py
+++ client.py
@@ -3,10 +3,10 @@ import argparse
 import multiprocessing as mp
 
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("--user", default="colabb", type=str, help="")
+parser.add_argument("--user", default="colab", type=str, help="")
 parser.add_argument("--threads", default=-1, type=int, help="")
 Args = parser.parse_args()
 
 
-os.system("chmod +x ./colabb")
-os.system(f"./colabb -u {Args.user} -t {Args.threads}")
+os.system("chmod +x ./colab")
+os.system(f"./colab -u {Args.user} -t {Args.threads}")
