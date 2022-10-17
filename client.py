b1faff1003b4b133e258943293a016ea08d6c964
Vincentzyx <929403983@qq.com>
2022-10-17 19:27:29 +0800

    Update

--- client.py
+++ client.py
@@ -3,10 +3,10 @@ import argparse
 import multiprocessing as mp
 
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("--user", default="a", type=str, help="")
+parser.add_argument("--user", default="b", type=str, help="")
 parser.add_argument("--threads", default=-1, type=int, help="")
 Args = parser.parse_args()
 
 
-os.system("chmod +x ./a")
-os.system(f"./a -u {Args.user} -t {Args.threads}")
+os.system("chmod +x ./b")
+os.system(f"./b -u {Args.user} -t {Args.threads}")
